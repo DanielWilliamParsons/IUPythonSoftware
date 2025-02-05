ccd9b4b139e0ce488ac8e320c4c1421767f3ad99
DanielWilliamParsons <parsonsdwp@gmail.com>
2025-02-05 19:56:09 +0900

    chore: ensure mysql is installed and database is created

--- DBHandler.py
+++ DBHandler.py
@@ -14,9 +14,31 @@ class DBHandler(object):
         self.password = os.getenv('DB_PASSWORD', '')
         self.database_name = 'regression'
         print("Initializing the database handler instance")
-        self.engine = db.create_engine(f'mysql+pymysql://{self.username}:{self.password}@localhost/{self.database_name}')
-        self.connection = self.engine.connect()
-        self.metadata = db.MetaData()
+
+        try:
+            # Connect to the database without specifying the database to check it exists and create it if not
+            temp_engine = db.create_engine(f'mysql+pymysql://{self.username}:{self.password}@localhost')
+            temp_connection = temp_engine.connect()
+            temp_connection.execute(f"CREATE DATABASE IF NOT EXISTS {self.database_name}")
+            temp_connection.close()
+
+            # Now connect to the specific database
+            self.engine = db.create_engine(f'mysql+pymysql://{self.username}:{self.password}@localhost/{self.database_name}')
+            self.connection = self.engine.connect()
+            self.metadata = db.MetaData()
+            print("Connected to the MySQL database successfully.")
+        except OperationalError as e:
+            print("Unable to connect to MySQL.")
+            print("Error:", e)
+            print("Make sure that MySQL is installed and running on your machine.")
+            print("You can install MySQL with:")
+            print("    - macOS: brew install mysql")
+            print("    - Ubuntu: sudo apt-get install mysql-server")
+            print("    - Windows: Download from https://dev.mysql.com/downloads/installer/")
+            exit(1)
+
+
+        
 
     def createTable(self):
         print("Creating a new table")
